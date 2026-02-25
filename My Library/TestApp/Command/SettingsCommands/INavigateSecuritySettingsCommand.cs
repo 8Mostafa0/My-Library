89a19dc644a05d53c08d2064bb9810f1c279518d
8mostafa0 <mosielite@gmail.com>
2026-02-25 17:10:02 +0330

    create NavigateSecuritySettingsCommand interface

---
+++ My Library/TestApp/Command/SettingsCommands/INavigateSecuritySettingsCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace My_Library.Command.SettingsCommands
+{
+    public interface INavigateSecuritySettingsCommand
+    {
+        void Execute(object? parameter);
+    }
+}
--- My Library/TestApp/Command/SettingsCommands/NavigateSecuritySettingsCommand.cs
+++ My Library/TestApp/Command/SettingsCommands/NavigateSecuritySettingsCommand.cs
@@ -3,7 +3,7 @@ using My_Library.ViewModel.SettingsViewModel;
 
 namespace My_Library.Command.SettingsCommands
 {
-    public class NavigateSecuritySettingsCommand : CommandBase
+    public class NavigateSecuritySettingsCommand : CommandBase, INavigateSecuritySettingsCommand
     {
         #region Dependencies
         private ISettingNavigationStore _settingNavigationStore;
